64c673c4874fdd94c6b1c637380c01ee32937976
tiagoaraujo00 <tiagodevjs@gmail.com>
2023-10-13 09:30:50 -0300

    enter key search implemented

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -2,23 +2,32 @@ type searchProps = {
   loadUser: (userName: string) => Promise<void>;
 };
 
-import { useState } from "react";
+import { useState, KeyboardEvent } from "react";
 
 import { BsSearch } from "react-icons/bs";
 
+import classes from './Search.module.css'
+
 function Search({ loadUser }: searchProps) {
   const [userName, setUserName] = useState("");
+
+  const handleKeyDown = (e: KeyboardEvent) => {
+    if(e.key === 'Enter') {
+      loadUser(userName)
+    }
+  }
   return (
-    <div>
+    <div className={classes.search}>
       <h2>Busque por um usuário:</h2>
       <p>Conheça seus melhores repositórios</p>
-      <div>
+      <div className={classes.search_container}>
         <input
           type="text"
           name=""
           id=""
           placeholder="Digite o nome do usuário"
           onChange={(e) => setUserName(e.target.value)}
+          onKeyDown={handleKeyDown}
         />
         <button onClick={() => loadUser(userName)}>
           <BsSearch />
